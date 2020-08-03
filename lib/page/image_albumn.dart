e118f072b89f7b73def225f9db547f6f40d0c091
fxt0706 <fxt0706@gmail.com>
2020-08-03 22:29:00 +0800

    add new pages

---
+++ lib/page/image_albumn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImageAlbumnPage extends StatefulWidget {
+  @override
+  _ImageAlbumnPageState createState() => _ImageAlbumnPageState();
+}
+
+class _ImageAlbumnPageState extends State<ImageAlbumnPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/sidepage/guess_like.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GuessLikePage extends StatefulWidget {
+  @override
+  _GuessLikePageState createState() => _GuessLikePageState();
+}
+
+class _GuessLikePageState extends State<GuessLikePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
