de68ed87a99fd26079e01882616153ea8d1cd887
fxt0706 <fxt0706@gmail.com>
2020-08-05 01:25:02 +0800

    edit guess like page

--- lib/widget/image_display.dart
+++ lib/widget/image_display.dart
@@ -5,6 +5,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter_advanced_networkimage/provider.dart';
+import 'package:dio/dio.dart';
 
 import '../page/pic_detail_page.dart';
 import '../data/common.dart';
@@ -66,9 +67,8 @@ Widget imageCell(Map picMapData, RandomColor randomColor, int sanityLevel,
                         url,
                         header: {'Referer': 'https://app-api.pixiv.net'},
                         useDiskCache: true,
-                        cacheRule: CacheRule(
-                            maxAge:
-                                Duration(days: previewRule)),
+                        cacheRule:
+                            CacheRule(maxAge: Duration(days: previewRule)),
                       ),
                       fit: BoxFit.fill,
                       frameBuilder:
@@ -93,7 +93,7 @@ Widget imageCell(Map picMapData, RandomColor randomColor, int sanityLevel,
             ),
           ),
           Positioned(
-            child: numberViewer(_picMainParameter(index)[1]),
+            child: numberViewer(number),
             right: ScreenUtil().setWidth(10),
             top: ScreenUtil().setHeight(5),
           ),
@@ -101,9 +101,92 @@ Widget imageCell(Map picMapData, RandomColor randomColor, int sanityLevel,
               ? Positioned(
                   bottom: ScreenUtil().setHeight(5),
                   right: ScreenUtil().setWidth(5),
-                  child: bookmarkHeart(index))
+                  child: bookmarkHeart(picMapData))
               : Container(),
         ],
       ),
     );
+}
+
+Widget numberViewer(num numberOfPic) {
+  return (numberOfPic != 1)
+      ? Container(
+          padding: EdgeInsets.all(ScreenUtil().setWidth(2)),
+          decoration: BoxDecoration(
+              color: Colors.black38, borderRadius: BorderRadius.circular(3)),
+          child: Row(
+            children: <Widget>[
+              Icon(
+                Icons.content_copy,
+                color: Colors.white,
+                size: ScreenUtil().setWidth(10),
+              ),
+              Text(
+                '$numberOfPic',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: ScreenUtil().setHeight(10),
+                    decoration: TextDecoration.none),
+              ),
+            ],
+          ),
+        )
+      : Container();
+}
+
+Widget bookmarkHeart(Map picMapData) {
+  bool isLikedLocalState = picMapData['isLiked'];
+  var color = isLikedLocalState ? Colors.redAccent : Colors.grey[300];
+  String picId = picMapData['id'].toString();
+
+  return AnimatedContainer(
+    duration: Duration(milliseconds: 500),
+    curve: Curves.fastLinearToSlowEaseIn,
+    alignment: Alignment.center,
+    // color: Colors.white,
+    height: isLikedLocalState
+        ? ScreenUtil().setWidth(33)
+        : ScreenUtil().setWidth(27),
+    width: isLikedLocalState
+        ? ScreenUtil().setWidth(33)
+        : ScreenUtil().setWidth(27),
+    child: GestureDetector(
+      onTap: () async {
+        String url = 'https://api.pixivic.com/users/bookmarked';
+        Map<String, String> body = {
+          'userId': prefs.getInt('id').toString(),
+          'illustId': picId.toString(),
+          'username': prefs.getString('name')
+        };
+        Map<String, String> headers = {
+          'authorization': prefs.getString('auth')
+        };
+        try {
+          if (isLikedLocalState) {
+            await Dio().delete(
+              url,
+              options: Options(
+                headers: headers,
+              ),
+              queryParameters: body,
+            );
+          } else {
+            await Dio().post(
+              url,
+              queryParameters: body,
+              options: Options(
+                headers: headers,
+              ),
+            );
+          }
+        } catch (e) {
+          print(e);
+        }
+      },
+      child: LayoutBuilder(builder: (context, constraint) {
+        return Icon(Icons.favorite,
+            color: color, size: constraint.biggest.height);
+      }),
+    ),
+  );
 }
