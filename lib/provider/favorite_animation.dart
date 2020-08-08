f793f8483edef76bb771ebc216eaa41e4143cffa
jy6c9w08 <yzc727824984@gmail.com>
2020-08-08 09:43:21 +0800

    add favoriteIcon animation provider

---
+++ lib/provider/favorite_animation.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil.dart';
+
+class FavProvider extends State<StatefulWidget>
+    with ChangeNotifier, TickerProviderStateMixin {
+  //登录按钮动画
+  Animation<double> _favAnimation;
+  AnimationController animationController;
+  CurvedAnimation curvedAnimation;
+  double _iconSize=30.0;
+  double get iconSize=>_iconSize;
+  Animation<double> get favAnimation=>_favAnimation;
+  clickFunc() {
+    animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 600));
+    curvedAnimation =
+        CurvedAnimation(parent: animationController, curve: Curves.easeInBack);
+    _favAnimation = Tween(begin: 30.0, end: 10.0).animate(curvedAnimation)
+      ..addListener(() {
+        _iconSize=_favAnimation.value;
+//        print(_favAnimation.value);
+        notifyListeners();
+      });
+    animationController.addStatusListener((status) {
+      if(status==AnimationStatus.completed){
+        animationController.reverse();
+        notifyListeners();
+      }
+    });
+    animationController.forward(from: 0);
+
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+
+  @override
+  void dispose() {
+//    animationController.dispose();
+    super.dispose();
+  }
+
+}
+
+
