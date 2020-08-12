d378619c493133b3fe56c8379d012f36eff3d311
jy6c9w08 <yzc727824984@gmail.com>
2020-08-12 09:16:13 +0800

    add searchbarprovider

---
+++ lib/provider/searchbar_height.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SearchBarHeightProvider with ChangeNotifier{
+  double _height=77;
+  double get height=>_height;
+  void changeHeight(double height){
+    _height=height;
+    notifyListeners();
+  }
+
+}
