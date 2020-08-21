b199443166d1aa32d41a77aeac05d319c3d50fa2
fxt0706 <fxt0706@gmail.com>
2020-08-22 00:55:26 +0800

    fix sugeestionbar error and some format

--- lib/provider/get_comment_list_data.dart
+++ lib/provider/get_comment_list_data.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:bot_toast/bot_toast.dart';
+
 class GetCommentProvider with ChangeNotifier{
   List _commentsList;
   List get commentList=>_commentsList;
   loadComments(int illustId) async {
-    String url = 'https://api.pixivic.com/illusts/${illustId}/comments';
+    String url = 'https://api.pixivic.com/illusts/$illustId/comments';
     var dio = Dio();
     Response response = await dio.get(url);
     if (response.statusCode == 200 && response.data['data'] != null) {
