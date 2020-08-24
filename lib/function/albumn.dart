9b4683490d5be2699e6ea13da986c48d5be1fec8
fxt0706 <fxt0706@gmail.com>
2020-08-25 02:27:47 +0800

    add albumn func

--- lib/function/albumn.dart
+++ lib/function/albumn.dart
@@ -9,7 +9,6 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
-import '../widget/papp_bar.dart';
 import '../data/common.dart';
 import '../data/texts.dart';
 
@@ -80,68 +79,150 @@ showAddNewAlbumnDialog(BuildContext context) {
   showDialog(
       context: context,
       builder: (BuildContext context) {
-        return Consumer<NewAlbumnBoolModel>(
-          builder: (context, NewAlbumnBoolModel newAlbumnBoolModel, child) => AlertDialog(
+        return Consumer<NewAlbumnParameterModel>(
+          builder: (context, NewAlbumnParameterModel newAlbumnParameterModel,
+                  child) =>
+              AlertDialog(
+            contentPadding: EdgeInsets.all(0),
             content: Container(
+              alignment: Alignment.topCenter,
               width: ScreenUtil().setWidth(260),
-              height: ScreenUtil().setHeight(250),
-              child: Column(
+              height: ScreenUtil().setHeight(280),
+              child: Stack(
                 children: [
-                  Container(
+                  Positioned(
+                    width: ScreenUtil().setWidth(260),
+                    top: ScreenUtil().setHeight(10),
+                    child: Column(
+                      children: [
+                        Container(
+                            width: ScreenUtil().setWidth(260),
+                            height: ScreenUtil().setHeight(25),
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.only(
+                                bottom: ScreenUtil().setHeight(8)),
+                            child: Text(
+                              '新建画集',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.orangeAccent),
+                            )),
+                        Container(
+                          width: ScreenUtil().setWidth(260),
+                          height: ScreenUtil().setHeight(30),
+                          child: TextField(
+                            cursorColor: Colors.orange,
+                            controller: title,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              isDense: true,
+                              hintText: texts.inputAlbumnTitle,
+                              hintStyle: TextStyle(
+                                  fontSize: 16, color: Colors.grey[300]),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: ScreenUtil().setWidth(260),
+                          child: TextField(
+                            cursorColor: Colors.orange,
+                            controller: caption,
+                            maxLines: 3,
+                            minLines: 1,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              isDense: true,
+                              hintText: texts.inputAlbumnCaption,
+                              hintStyle: TextStyle(
+                                  fontSize: 16, color: Colors.grey[300]),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: ScreenUtil().setWidth(200),
+                          height: ScreenUtil().setHeight(30),
+                          child: SwitchListTile(
+                            value: newAlbumnParameterModel.isPublic,
+                            dense: true,
+                            onChanged: (value) {
+                              newAlbumnParameterModel.public(value);
+                            },
+                            activeColor: Colors.orangeAccent,
+                            title: Text(
+                              '公开画集',
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: ScreenUtil().setWidth(200),
+                          height: ScreenUtil().setHeight(30),
+                          child: SwitchListTile(
+                            value: newAlbumnParameterModel.isSexy,
+                            dense: true,
+                            onChanged: (value) {
+                              newAlbumnParameterModel.sexy(value);
+                            },
+                            activeColor: Colors.orangeAccent,
+                            title:
+                                Text('R16内容', style: TextStyle(fontSize: 14)),
+                          ),
+                        ),
+                        Container(
+                          width: ScreenUtil().setWidth(200),
+                          height: ScreenUtil().setHeight(30),
+                          child: SwitchListTile(
+                            value: newAlbumnParameterModel.allowComment,
+                            dense: true,
+                            onChanged: (value) {
+                              newAlbumnParameterModel.comment(value);
+                            },
+                            activeColor: Colors.orangeAccent,
+                            title: Text('允许评论', style: TextStyle(fontSize: 14)),
+                          ),
+                        ),
+                        FlatButton(
+                          shape: StadiumBorder(),
+                          onPressed: () {
+                            showTagSelector(context);
+                          },
+                          child: Text('添加标签'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    width: ScreenUtil().setWidth(260),
+                    bottom: ScreenUtil().setHeight(8),
+                    child: Container(
                       alignment: Alignment.center,
-                      child: Text(
-                        '新建画集',
-                        style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w300,
-                            color: Colors.orangeAccent),
-                      )),
-                  TextField(
-                    controller: title,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      isDense: true,
-                      hintText: texts.inputAlbumnTitle,
-                      hintStyle:
-                          TextStyle(fontSize: 14, color: Colors.grey[300]),
+                      child: FlatButton(
+                        child: Text(
+                          '提交',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.orange[200],
+                        shape: StadiumBorder(),
+                        onPressed: () {
+                          Map<String, dynamic> payload = {
+                            'title': title.text,
+                            'caption': caption.text,
+                            'isPublic':
+                                newAlbumnParameterModel.isPublic ? 1 : 0,
+                            'pornWarning':
+                                newAlbumnParameterModel.isSexy ? 1 : 0,
+                            'forbidComment':
+                                newAlbumnParameterModel.allowComment ? 1 : 0,
+                          };
+                          postNewAlbumn(payload);
+                        },
+                      ),
                     ),
-                  ),
-                  TextField(
-                    controller: caption,
-                    maxLines: 5,
-                    minLines: 1,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      isDense: true,
-                      hintText: texts.inputAlbumnCaption,
-                      hintStyle:
-                          TextStyle(fontSize: 14, color: Colors.grey[300]),
-                    ),
-                  ),
-                  SwitchListTile(
-                    value: newAlbumnBoolModel.isPublic,
-                    onChanged: (value) {
-                      newAlbumnBoolModel.public(value);
-                    },
-                    activeColor: Colors.orangeAccent,
-                    title: Text('公开画集'),
-                  ),
-                  SwitchListTile(
-                    value: newAlbumnBoolModel.isSexy,
-                    onChanged: (value) {
-                      newAlbumnBoolModel.sexy(value);
-                    },
-                    activeColor: Colors.orangeAccent,
-                    title: Text('R16内容'),
-                  ),
-                  SwitchListTile(
-                    value: newAlbumnBoolModel.allowComment,
-                    onChanged: (value) {
-                      newAlbumnBoolModel.comment(value);
-                    },
-                    activeColor: Colors.orangeAccent,
-                    title: Text('允许评论'),
-                  ),
+                  )
                 ],
               ),
             ),
@@ -150,14 +231,95 @@ showAddNewAlbumnDialog(BuildContext context) {
       });
 }
 
-class NewAlbumnBoolModel with ChangeNotifier {
+showTagSelector(context) {
+  showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController tagInput = TextEditingController();
+        return Consumer<NewAlbumnParameterModel>(
+            builder: (context, NewAlbumnParameterModel newAlbumnParameterModel,
+                    child) =>
+                AlertDialog(
+                  content: Container(
+                      width: ScreenUtil().setWidth(324),
+                      height: ScreenUtil().setWidth(400),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: ScreenUtil().setWidth(280),
+                            child: TextField(
+                                controller: tagInput,
+                                decoration: InputDecoration(
+                                  hintText: '输入你想要添加的标签',
+                                  isDense: true,
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.grey)),
+                                ),
+                                onEditingComplete: () {
+                                  newAlbumnParameterModel
+                                      .getTagAdvice(tagInput.text);
+                                }),
+                          ),
+                          Wrap(
+                            children: newAlbumnParameterModel.tagsAdvice
+                                .map((item) => singleTag(item['tagName']))
+                                .toList(),
+                          )
+                        ],
+                      )),
+                ));
+      });
+}
+
+postNewAlbumn(Map<String, dynamic> payload) async {
+  // TODO: fill postNewAlbumn
+  // String url = 'https://api.pixivic.com/collections';
+  // Map<String, String> headers = {'authorization': prefs.getString('auth')};
+}
+
+Widget singleTag(String label) {
+  return Container(
+    padding: EdgeInsets.all(ScreenUtil().setWidth(3)),
+    child: ButtonTheme(
+      height: ScreenUtil().setHeight(20),
+      minWidth: ScreenUtil().setWidth(2),
+      buttonColor: Colors.grey[100],
+      splashColor: Colors.grey[100],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(13.0)),
+      child: OutlineButton(
+        onPressed: () {},
+        child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            Text(
+              label,
+              style: TextStyle(color: Colors.grey),
+            ),
+            Icon(
+              Icons.cancel,
+              color: Colors.grey,
+              size: ScreenUtil().setWidth(13),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+class NewAlbumnParameterModel with ChangeNotifier {
   bool _isPublic = true;
   bool _isSexy = false;
   bool _allowComment = true;
+  List _tags = [];
+  List _tagsAdvice = [];
 
   bool get isPublic => _isPublic;
   bool get isSexy => _isSexy;
   bool get allowComment => _allowComment;
+  List get tags => _tags;
+  List get tagsAdvice => _tagsAdvice;
 
   void public(bool result) {
     _isPublic = result;
@@ -172,5 +334,40 @@ class NewAlbumnBoolModel with ChangeNotifier {
   void comment(bool result) {
     _allowComment = result;
     notifyListeners();
+  }
+
+  void cleanTag() {
+    _tags = [];
+    notifyListeners();
+  }
+
+  void getTagAdvice(String keywords) async {
+    _tagsAdvice = [
+      {'tagName': keywords}
+    ];
+    notifyListeners();
+    String url = 'https://api.pixivic.com/collections/tags?keyword=$keywords';
+    Map<String, String> headers = {'authorization': prefs.getString('auth')};
+
+    try {
+      Response response =
+          await Dio().get(url, options: Options(headers: headers));
+      _tagsAdvice = response.data['data'];
+      print(_tagsAdvice);
+      notifyListeners();
+      // _tagsAdvice = [];
+    } on DioError catch (e) {
+      if (e.response != null) {
+        BotToast.showSimpleNotification(title: e.response.data['message']);
+        print(e.response.data);
+        print(e.response.headers);
+        print(e.response.request);
+      } else {
+        // Something happened in setting up or sending the request that triggered an Error
+        BotToast.showSimpleNotification(title: e.message);
+        print(e.request);
+        print(e.message);
+      }
+    }
   }
 }
