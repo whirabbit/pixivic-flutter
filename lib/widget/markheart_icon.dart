ad4b1da9fdad31a82926b348a4f146d9a426a882
jy6c9w08 <yzc727824984@gmail.com>
2020-09-01 17:33:10 +0800

    分离mark组件,使用provider

---
+++ lib/widget/markheart_icon.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:pixivic/data/common.dart';
+import 'package:pixivic/provider/get_page.dart';
+
+import 'package:requests/requests.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/screenutil.dart';
+
+import 'package:pixivic/provider/favorite_animation.dart';
+
+class MarkHeart extends StatelessWidget {
+  MarkHeart(
+      {@required this.picItem,
+      @required this.index,
+      @required this.getPageProvider});
+
+  final Map picItem;
+  final int index;
+
+  final GetPageProvider getPageProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<FavProvider>(
+          create: (_) => FavProvider(),
+        )
+      ],
+      child: Consumer<FavProvider>(
+        builder: (context, FavProvider favProvider, _) {
+          bool isLikedLocalState = picItem['isLiked'];
+          Color color = isLikedLocalState ? Colors.redAccent : Colors.grey[300];
+          String picId = picItem['id'].toString();
+          return IconButton(
+            color: color,
+            padding: EdgeInsets.all(0),
+            iconSize: ScreenUtil().setHeight(favProvider.iconSize),
+            icon: Icon(Icons.favorite),
+            onPressed: () async {
+              //点击动画
+              favProvider.clickFunc();
+              String url = 'https://api.pixivic.com/users/bookmarked';
+              Map<String, String> body = {
+                'userId': prefs.getInt('id').toString(),
+                'illustId': picId.toString(),
+                'username': prefs.getString('name')
+              };
+              Map<String, String> headers = {
+                'authorization': prefs.getString('auth')
+              };
+              try {
+                if (isLikedLocalState) {
+                  await Requests.delete(url,
+                      body: body,
+                      headers: headers,
+                      bodyEncoding: RequestBodyEncoding.JSON);
+                } else {
+                  await Requests.post(url,
+                      body: body,
+                      headers: headers,
+                      bodyEncoding: RequestBodyEncoding.JSON);
+                }
+                Future.delayed(Duration(milliseconds: 400), () {
+//                      setState(() {
+                  getPageProvider.flipLikeState(index);
+
+//                      });
+                });
+              } catch (e) {
+                print(e);
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
