53b52778cc296a992e0743c6617e0c0f158cddd8
jy6c9w08 <yzc727824984@gmail.com>
2020-09-03 23:16:51 +0800

    修复评论自动加载

--- lib/provider/get_comment_list_data.dart
+++ lib/provider/get_comment_list_data.dart
@@ -2,24 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:bot_toast/bot_toast.dart';
 
-class GetCommentProvider with ChangeNotifier{
-  List _commentsList;
-  List get commentList=>_commentsList;
-  loadComments(int illustId) async {
-    String url = 'https://api.pixivic.com/illusts/$illustId/comments';
+class GetCommentProvider with ChangeNotifier {
+  List commentList;
+
+  loadComments(int illustId, {int page}) async {
+    String url =
+        'https://api.pixivic.com/illusts/$illustId/comments?page=$page&pageSize=10';
     var dio = Dio();
     Response response = await dio.get(url);
     if (response.statusCode == 200 && response.data['data'] != null) {
       // print(response.data);
-
-      _commentsList = response.data['data'];
-        notifyListeners();
+      commentList = response.data['data'];
+      notifyListeners();
+      return commentList;
     } else if (response.statusCode == 200 && response.data['data'] == null) {
+      commentList = [];
       print('comments: null but 200');
+      return commentList;
     } else {
       BotToast.showSimpleNotification(title: response.data['message']);
     }
   }
+
   @override
   void dispose() {
     super.dispose();
