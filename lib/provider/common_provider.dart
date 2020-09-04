817f16eb0350bce6f5ffdb8e8856b2b0bcf5974b
jy6c9w08 <yzc727824984@gmail.com>
2020-09-04 22:48:49 +0800

    更新

--- lib/provider/common_provider.dart
+++ lib/provider/common_provider.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 
-class CommentProvider with ChangeNotifier {
+class CommonProvider with ChangeNotifier {
   double _height = 77;
+
   double get height => _height;
-  bool detailPageIsLike;
-//  bool get detailPageIsLike=> _detailPageIsLike;
 
   void changeHeight(double height) {
     _height = height;
-    notifyListeners();
-  }
-  void changeIsLike(bool isLike) {
-    detailPageIsLike=isLike;
     notifyListeners();
   }
 }
