802d3de34467603284891106300b9ce1f170cd95
jy6c9w08 <yzc727824984@gmail.com>
2020-10-14 17:28:30 +0800

    修复mark同步

--- lib/page/pic_page.dart
+++ lib/page/pic_page.dart
@@ -323,6 +323,7 @@ class _PicPageState extends State<PicPage> {
           print('Selector11111111111111111111');
           pageModel.context = context;
           return Selector<PicPageModel, Tuple2<List, bool>>(
+              shouldRebuild: (pre, next) => true,
               selector: (BuildContext context, PicPageModel provider) {
             return Tuple2(provider.picList, provider.hasConnected);
           }, builder: (context, tuple, _) {
@@ -373,7 +374,6 @@ class _PicPageState extends State<PicPage> {
                     crossAxisCount: 2,
                     itemCount: pageModel.picList.length,
                     itemBuilder: (BuildContext context, int index) {
-                      print(pageModel.picList[index]['artistId']);
                       return imageCell(pageModel.picList[index], index, context, pageModel);
                     },
                     staggeredTileBuilder: (index) => StaggeredTile.fit(1),
@@ -400,7 +400,6 @@ class _PicPageState extends State<PicPage> {
 
   Widget imageCell(
       Map picItem, int index, BuildContext context, PicPageModel pageModel) {
-    print('imageCell刷新！！！！！！！！！！');
     final Color color = RandomColor().randomColor();
     Map picMapData = Map.from(picItem);
     if (picMapData['xrestict'] == 1 ||
