01427eeb0730b3f2df78a27fb2c5c872db22a6fd
fxt0706 <fxt0706@gmail.com>
2020-11-01 23:15:50 +0800

    feature - empty collection display card

--- lib/page/pic_detail_page.dart
+++ lib/page/pic_detail_page.dart
@@ -533,7 +533,7 @@ class _PicDetailPageState extends State<PicDetailPage> {
             color: Colors.blueGrey,
           ),
           onTap: () {
-            showAddToCollection(context, [widget._picData['id']]);
+            showAddToCollection(context, [widget._picData['id']], multiSelect: false);
           },
         ),
       ),
