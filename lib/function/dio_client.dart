f78de83da329e088d4384aa68203ef33e6f8543b
fxt0706 <fxt0706@gmail.com>
2020-11-14 16:33:32 +0800

    feature - add thumbup in comment but not work right

--- lib/function/dio_client.dart
+++ lib/function/dio_client.dart
@@ -32,13 +32,13 @@ initDioClient() {
       print(e.response.data);
       print(e.response.headers);
       print(e.response.request);
-      return e.response;
+      return false;
     } else {
       // Something happened in setting up or sending the request that triggered an Error
       BotToast.showSimpleNotification(title: e.message);
       print(e.request);
       print(e.message);
-      return e;
+      return false;
     }
   }));
 }
