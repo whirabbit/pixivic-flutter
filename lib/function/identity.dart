83ff8987a05f44bf8462ee2884ee2fca33e19d82
fxt0706 <fxt0706@gmail.com>
2020-12-20 00:26:21 +0800

    fix - collection load fail after relogin

--- lib/function/identity.dart
+++ lib/function/identity.dart
@@ -67,7 +67,7 @@ login(BuildContext context, String userName, String pwd,
     homePicList = [];
     homeCurrentPage = 1;
     // 加载用户的画集列表
-    Provider.of<CollectionUserDataModel>(context, listen: false);
+    Provider.of<CollectionUserDataModel>(context, listen: false).getCollectionList();
   } else {
     // isLogin = false;
     BotToast.showSimpleNotification(
