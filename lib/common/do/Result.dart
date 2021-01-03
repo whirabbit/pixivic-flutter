d88f8aaf6d526708334796a3a2e519e266222899
jy6c9w08 <yzc727824984@gmail.com>
2021-01-03 11:54:27 +0800

    add Result model

---
+++ lib/common/do/Result.dart
@@ -0,0 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'Result.g.dart';
+
+@JsonSerializable()
+class Result<T> {
+  String message;
+
+  dynamic data;
+
+  Result({this.message, this.data});
+
+  factory Result.fromJson(Map<String, dynamic> json) => _$ResultFromJson(json);
+  Map<String, dynamic> toJson() => _$ResultToJson(this);
+
+
+}
+
