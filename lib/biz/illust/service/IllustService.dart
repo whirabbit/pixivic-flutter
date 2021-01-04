7b4365174237604c5ebeb394933c76b15cf332fe
jy6c9w08 <yzc727824984@gmail.com>
2021-01-04 19:10:25 +0800

    添加illust相关的Client,service

---
+++ lib/biz/illust/service/IllustService.dart
@@ -0,0 +1,113 @@
+import 'package:injectable/injectable.dart';
+import 'package:pixivic/http/client/IllustRestClient.dart';
+
+import 'package:pixivic/common/do/Illust.dart';
+import 'package:pixivic/common/do/Result.dart';
+
+@lazySingleton
+class IllustService {
+  final IllustRestClient _illustRestClient;
+
+  IllustService(this._illustRestClient);
+
+  processData(List data) {
+    List<Illust> illustList = [];
+    data.map((s) => Illust.fromJson(s)).forEach((e) {
+      illustList.add(e);
+    });
+    return illustList;
+  }
+
+  Future<Result<List<Illust>>> queryIllustRank(
+      String date, String mode, int page, int pageSize) {
+    return _illustRestClient
+        .queryRank(date, mode, page, pageSize)
+        .then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustRelated(
+      num relatedId, int page, int pageSize) {
+    return _illustRestClient
+        .queryRelated(relatedId, page, pageSize)
+        .then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustSearch(
+      String keyword, int page, int pageSize) {
+    return _illustRestClient.querySearch(page, keyword, pageSize).then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustArtist(String artistId, String type,
+      int page, int pageSize, int maxSanityLevel) {
+    return _illustRestClient
+        .queryArtist(artistId, type, page, pageSize, maxSanityLevel)
+        .then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustFollowed(
+      String userId, String type, int page, int pageSize) {
+    return _illustRestClient
+        .queryFollowed(userId, type, page, pageSize)
+        .then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustBookmark(
+      String userId, String type, int page, int pageSize) {
+    return _illustRestClient
+        .queryBookmark(userId, type, page, pageSize)
+        .then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustSpotlight(String spotlightId) {
+    return _illustRestClient.querySpotlight(spotlightId).then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustHistory(
+      String userId, int page, int pageSize) {
+    return _illustRestClient.queryHistory(userId, page, pageSize).then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustOldHistory(
+      String userId, int page, int pageSize) {
+    return _illustRestClient
+        .queryOldHistory(userId, page, pageSize)
+        .then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+
+  Future<Result<List<Illust>>> queryIllustCollection(
+      String collectionId, int page, int pageSize) {
+    return _illustRestClient
+        .queryCollection(collectionId, page, pageSize)
+        .then((value) {
+      if (value.data != null) value.data = processData(value.data);
+      return value;
+    });
+  }
+}
---
+++ lib/http/client/IllustRestClient.dart
@@ -0,0 +1,80 @@
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/http.dart';
+import 'package:dio/dio.dart';
+
+import 'package:pixivic/common/do/Illust.dart';
+import 'package:pixivic/common/do/Result.dart';
+
+part 'IllustRestClient.g.dart';
+
+@lazySingleton
+@RestApi(baseUrl: "https://pix.ipv4.host")
+abstract class IllustRestClient {
+  @factoryMethod
+  factory IllustRestClient(Dio dio, {@Named("baseUrl") String baseUrl}) =
+      _IllustRestClient;
+
+  @GET("/ranks")
+  Future<Result<List<Illust>>> queryRank(
+      @Query("date") String date,
+      @Query("mode") String mode,
+      @Query("page") int page,
+      @Query("pageSize") int pageSize);
+
+  @GET("/illusts/{illustId}/related")
+  Future<Result<List<Illust>>> queryRelated(@Path("illustId") num illustId,
+      @Query("page") int page, @Query("pageSize") int pageSize);
+
+  @GET("/illustrations")
+  Future<Result<List<Illust>>> querySearch(@Query("page") int page,
+      @Query("keyword") String keyword, @Query("pageSize") int pageSize);
+
+  @GET("/artists/{artistId}/illusts/{type}")
+  Future<Result<List<Illust>>> queryArtist(
+      @Path("artistId") String artistId,
+      @Path("type") String type,
+      @Query("page") int page,
+      @Query("pageSize") int pageSize,
+      @Query("maxSanityLevel") int maxSanityLevel);
+
+  @GET("/users/{userId}/followed/latest/{type}")
+  Future<Result<List<Illust>>> queryFollowed(
+    @Path("userId") String userId,
+    @Path("type") String type,
+    @Query("page") int page,
+    @Query("pageSize") int pageSize,
+  );
+
+  @GET("/users/{userId}/bookmarked/{type}")
+  Future<Result<List<Illust>>> queryBookmark(
+    @Path("userId") String userId,
+    @Path("type") String type,
+    @Query("page") int page,
+    @Query("pageSize") int pageSize,
+  );
+
+  @GET("/spotlights/{spotlightId}/illustrations")
+  Future<Result<List<Illust>>> querySpotlight(
+      @Path("spotlightId") String userId);
+
+  @GET("/users/{userId}/illustHistory")
+  Future<Result<List<Illust>>> queryHistory(
+    @Path("userId") String userId,
+    @Query("page") int page,
+    @Query("pageSize") int pageSize,
+  );
+
+  @GET("/users/{userId}/oldIllustHistory")
+  Future<Result<List<Illust>>> queryOldHistory(
+    @Path("userId") String userId,
+    @Query("page") int page,
+    @Query("pageSize") int pageSize,
+  );
+
+  @GET("/collections/{collectionId}/illustrations")
+  Future<Result<List<Illust>>> queryCollection(
+    @Path("collectionId") String collectionId,
+    @Query("page") int page,
+    @Query("pageSize") int pageSize,
+  );
+}
