a3a38aca4ace61d641e55279630b02a518e69a56
jy6c9w08 <yzc727824984@gmail.com>
2021-01-04 12:38:18 +0800

    分离数据处理函数

--- lib/biz/comment/service/CommentService.dart
+++ lib/biz/comment/service/CommentService.dart
@@ -10,17 +10,20 @@ class CommentService {
 
   CommentService(this._commentRestClient);
 
-  Future<Result<List<Comment>>> queryCommentInfo(String illusts,
-      int illustId, int page, int pageSize) {
+  processData(List data) {
+    List<Comment> illustList = [];
+    data.map((s) => Comment.fromJson(s)).forEach((e) {
+      illustList.add(e);
+    });
+    return illustList;
+  }
+
+  Future<Result<List<Comment>>> queryCommentInfo(
+      String illusts, int illustId, int page, int pageSize) {
     return _commentRestClient
-        .queryCommentInfo(illusts,illustId, page, pageSize)
+        .queryCommentInfo(illusts, illustId, page, pageSize)
         .then((value) {
-      List<Comment> commentList = [];
-      if(value.data!=null)
-      value.data.map((s) => Comment.fromJson(s)).toList().forEach((e) {
-        commentList.add(e);
-      });
-      value.data = commentList;
+      if (value.data != null) value.data = processData(value.data);
       return value;
     });
   }
