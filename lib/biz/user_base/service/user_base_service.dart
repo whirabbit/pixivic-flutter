2f4f33f4ead5349b948b77a4dcc7780c4672157e
jy6c9w08 <yzc727824984@gmail.com>
2021-01-07 21:48:34 +0800

    添加 userBase Service

---
+++ lib/biz/user_base/service/user_base_service.dart
@@ -0,0 +1,45 @@
+import 'package:injectable/injectable.dart';
+import 'package:pixivic/data/texts.dart';
+import 'package:pixivic/http/client/user_base_rest_client.dart';
+import 'package:dio/dio.dart';
+
+import 'package:pixivic/common/do/user_info.dart';
+import 'package:bot_toast/bot_toast.dart';
+
+@lazySingleton
+class UserBaseService {
+  final UserBaseRestClient _userBaseRestClient;
+
+  UserBaseService(this._userBaseRestClient);
+
+  processDioError(obj) {
+    final res = (obj as DioError).response;
+    BotToast.showSimpleNotification(title: res.statusMessage);
+  }
+
+  Future<UserInfo> queryUserLogin(String vid, String code, Map body) {
+    return _userBaseRestClient
+        .queryUserLoginInfo(vid, code, body)
+        .then((value) {
+      if (value.data != null) value.data = UserInfo.fromJson(value.data);
+      return value.data;
+    });
+  }
+
+  Future<String> queryUserRegisters(String vid, String code, Map body) {
+    return _userBaseRestClient
+        .queryUserRegistersInfo(vid, code, body)
+        .then((value) {
+      if (value.data != null) value.data = UserInfo.fromJson(value.data);
+      BotToast.showSimpleNotification(title: TextZhLoginPage().registerSucceed);
+      return value.data;
+    }).catchError((Object obj) {
+      switch (obj.runtimeType) {
+        case DioError:
+          processDioError(obj);
+          break;
+        default:
+      }
+    });
+  }
+}
